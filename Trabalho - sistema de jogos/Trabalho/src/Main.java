c6fe973af3086e720c07ea97ef4e1aa45e81ea8c
Yasmin Lopes <146793686+yaslopes07@users.noreply.github.com>
2026-06-02 20:43:12 -0300

    Delete Trabalho - sistema de jogos/Trabalho directory

--- Trabalho - sistema de jogos/Trabalho/src/Main.java
+++
@@ -1,9 +0,0 @@
-import View.MenuView;
-
-public class Main {
-    public static void main(String[] args) {
-        // Inicializa o menu 
-        MenuView menu = new MenuView();
-        menu.exibirMenuPrincipal();
-    }
-}
