38772f8f0e52e79ec39b21ac5aa14c84a056e96f
marianapoloantonio-byte <marianapoloantonio@gmail.com>
2026-06-07 19:48:33 -0300

    Update Pessoa.java

--- Model/Pessoa.java
+++ Model/Pessoa.java
@@ -2,45 +2,46 @@ package Model;
 
 import java.io.Serializable;
 
-public abstract class Pessoa implements Serializable{
+    public abstract class Pessoa implements Serializable{
 
-    protected int idade;
-    protected String nome;
-    protected String email;
+        protected int id;
+        protected String nome;
+        protected String email;
 
 
-    public Pessoa (int idade, String nome, String email){
-        this.idade = idade;
-        this.nome = nome;
-        this.email = email;
+        public Pessoa (int id, String nome, String email){
+            this.id = id;
+            this.nome = nome;
+            this.email = email;
+        }
+
+        //abstrato
+        public abstract void exibirPerfil();
+
+        public int getIdade() {
+            return id;
+        }
+
+        public String getNome() {
+            return nome;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setIdade(int id) {
+            this.id = id;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+
     }
 
-    //abstrato
-    public abstract void exibirPerfil();
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-}
