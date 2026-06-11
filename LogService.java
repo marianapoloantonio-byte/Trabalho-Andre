abf22cefbde8348fff89109bda8cdd784ccd02d9
Yasmin Lopes <146793686+yaslopes07@users.noreply.github.com>
2026-06-11 19:24:16 -0300

    Add files via upload

---
+++ LogService.java
@@ -0,0 +1,26 @@
+package Service;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+// -- Serve para registrar em um arquivo de texto
+public class LogService {
+
+    public static void registrar(String mensagem) {
+
+        try {
+
+            FileWriter writer = new FileWriter("log.txt", true);
+
+            writer.write(mensagem + "\n");
+
+            writer.close();
+
+        } catch (IOException e) {
+
+            System.out.println("Erro ao gravar log.");
+        }
+    }
+
+
+}
